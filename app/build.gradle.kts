efc7d3306c212e99f682e84e08c3fa6f6bc74e0a
nvlad <nvlad1@ukr.net>
2026-07-04 23:22:14 +0300

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "io.github.nvlad1.function3danimator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.4.1"
+        versionCode = 26
+        versionName = "1.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
