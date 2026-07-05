7c0bacbbda6c602ffd849d2085c8e4d5a947d211
nvlad <nvlad1@ukr.net>
2026-07-05 17:12:41 +0300

    Add free orientation

--- app/src/main/java/io/github/nvlad1/function3danimator/ui/settings/EnumMainActivityOrientation.kt
+++ app/src/main/java/io/github/nvlad1/function3danimator/ui/settings/EnumMainActivityOrientation.kt
@@ -1,5 +1,5 @@
 package io.github.nvlad1.function3danimator.ui.settings
 
 enum class EnumMainActivityOrientation(val description: String) {
-    portrait("Portrait"), landscape("Landscape")
+    portrait("Portrait"), landscape("Landscape"), free("Free")
 }
